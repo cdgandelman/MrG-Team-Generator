69205fb1732b61b61e7db3db8ab70a39b8e66a28
cdgandelman <cdgandelman@yahoo.com>
2023-03-21 14:00:07 -0600

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+// link to page creation
+const generateHTML = require('./src/generateHTML');
+
+// team profiles
+const Manager = require('./Libraries/Manager');
+const Engineer = require('./Libraries/Engineer');
+const Intern = require('./Libraries/Intern'); 
+
+// node modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// employee array
+const employeeArray = []; 
+
